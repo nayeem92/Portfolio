469e431806115108976a2144876f51ad31a7faea
Nayeem Khan <nayeem0073297@gmail.com>
2023-12-19 23:45:47 -0500

    hamburger

--- components/navbar.js
+++ components/navbar.js
@@ -130,7 +130,7 @@ const Navbar = props => {
                 </MenuItem> */}
                 <MenuItem
                   as={Link}
-                  href="https://github.com/craftzdog/craftzdog-homepage"
+                  href="https://github.com/nayeem92"
                 >
                   Github
                 </MenuItem>
